bce64a55e9cc8219a3b60eb2057926c6fb829dd6
euokyun <euokyun@me.com>
2025-01-08 22:02:41 +0900

    add tremotesf2

---
+++ Casks/tremotesf2.rb
@@ -0,0 +1,24 @@
+cask "tremotesf2" do
+  version "2.7.4"
+  sha256 :no_check
+
+  url "https://github.com/equeim/tremotesf2/releases/download/#{version.major}/tremotesf-#{version.major}-macOS-arm64-Release.dmg"
+  name "tremotesf2"
+  desc "Transmission remote client"
+  homepage "https://github.com/equeim/tremotesf2"
+
+  auto_updates false
+
+  app "Tremotesf.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args:         [
+                     "-dr",
+                     "com.apple.quarantine",
+                     "#{appdir}/Ghidra.app",
+                   ]
+  end
+
+  zap trash: []
+end
