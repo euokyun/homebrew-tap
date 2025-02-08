2d677a66cb8c7920ab69cdef6c536d6006b83635
euokyun <euokyun@me.com>
2025-02-08 22:37:21 +0900

    lowercase name

--- Casks/rimsort.rb
+++ Casks/rimsort.rb
@@ -1,10 +1,10 @@
-cask "RimSort" do
+cask "rimsort" do
   version "v1.0.11"
   arch arm: "arm", intel: "i386"
   sha256 :no_check
 
   url "https://github.com/RimSort/RimSort/releases/download/#{version}/RimSort-#{version}-Darwin_#{arch}.zip"
-  name "RimSort"
+  name "rimsort"
   desc ""
   homepage "https://github.com/RimSort/RimSort"
 
