21770515287c1528a9c6b6c96dc3c018ac25596a
euokyun <euokyun@me.com>
2025-05-28 12:52:23 +0900

    build: Add Yaagl Cask file

---
+++ Casks/yaagl.rb
@@ -0,0 +1,28 @@
+cask "yaagl" do
+  version '0.2.23'
+  sha256 :no_check
+
+  url "https://github.com/yaagl/yet-another-anime-game-launcher/releases/download/#{version}/Yaagl.OS.app.tar.gz"
+  name "Yaagl"
+  desc ""
+  homepage "https://github.com/yaagl/yet-another-anime-game-launcher"
+
+  auto_updates false
+
+  app "Yaagl OS.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args:         [
+                     "-dr",
+                     "com.apple.quarantine",
+                     "#{appdir}/Yaagl OS.app",
+                   ]
+  end
+
+  zap trash: [
+        "~/Library/Application Support/Yaagl OS",
+        "~/Library/Caches/com.3shain.yaagl.os",
+        "~/Library/WebKit/com.3shain.yaagl.os"
+      ]
+end
