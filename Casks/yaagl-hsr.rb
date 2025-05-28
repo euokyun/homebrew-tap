2aa7fce3153af1d7fbdfa463033481620a5fb584
euokyun <euokyun@me.com>
2025-05-28 10:57:20 +0900

    build: Add Casks for Yaagl HSR and ZZZ

---
+++ Casks/yaagl-hsr.rb
@@ -0,0 +1,29 @@
+cask "Yaagl HSR" do
+  version '0.2.24'
+  sha256 :no_check
+
+  url "https://github.com/yaagl/yet-another-anime-game-launcher/releases/download/#{version}/Yaagl.HSR.OS.app.tar.gz
+"
+  name "Yaagl HSR"
+  desc ""
+  homepage "https://github.com/yaagl/yet-another-anime-game-launcher"
+
+  auto_updates false
+
+  app "Yaagl HSR OS.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args:         [
+                     "-dr",
+                     "com.apple.quarantine",
+                     "#{appdir}/Yaagl HSR OS.app",
+                   ]
+  end
+
+  zap trash: [
+        "~/Library/Application Support/Yaagl HSR OS",
+        "~/Library/Caches/com.3shain.yaagl.hkrpg.os",
+        "~/Library/WebKit/com.3shain.yaagl.hkrpg.os"
+      ]
+end
---
+++ Casks/yaagl-zzz.rb
@@ -0,0 +1,29 @@
+cask "Yaagl ZZZ" do
+  version '0.2.24'
+  sha256 :no_check
+
+  url "https://github.com/yaagl/yet-another-anime-game-launcher/releases/download/#{version}/Yaagl.ZZZ.OS.app.tar.gz
+"
+  name "Yaagl ZZZ"
+  desc ""
+  homepage "https://github.com/yaagl/yet-another-anime-game-launcher"
+
+  auto_updates false
+
+  app "Yaagl ZZZ OS.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args:         [
+                     "-dr",
+                     "com.apple.quarantine",
+                     "#{appdir}/Yaagl ZZZ OS.app",
+                   ]
+  end
+
+  zap trash: [
+        "~/Library/Application Support/Yaagl ZZZ OS",
+        "~/Library/Caches/com.3shain.yaagl.nap.os",
+        "~/Library/WebKit/com.3shain.yaagl.nap.os"
+      ]
+end
